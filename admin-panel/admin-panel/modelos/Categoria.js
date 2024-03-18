201d69f67d517ef495fbfb4c179d081a632bb775
Dev-Samkrosh <samuelbp3005@gmail.com>
2024-03-18 11:50:21 -0500

    Se corrompieron los documentos

--- admin-panel/admin-panel/modelos/Categoria.js
+++
@@ -1,9 +0,0 @@
-import mongoose, {model, models, Schema} from "mongoose";
-
-const categoriaSchema = new Schema({
-  nombre: {type:String,required:true},
-  padre: {type:mongoose.Types.ObjectId, ref:'Categoria'},
-  propiedades: [{type:Object}]
-});
-
-export const Categoria = models?.Categoria || model('Categoria', categoriaSchema);
