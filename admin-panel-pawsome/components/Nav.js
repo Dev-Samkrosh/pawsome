e07ae3e424c12a32481a1cf802e6787a593b0f93
Dev-Samkrosh <samuelbp3005@gmail.com>
2024-03-26 07:00:41 -0500

    DISEÑO BÁSICO DE ADMIN

--- admin-panel-pawsome/components/Nav.js
+++ admin-panel-pawsome/components/Nav.js
@@ -1,19 +1,56 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 
 export default function Nav() {
+    const inactiveLink = "flex gap-1 p-1";
+    const activeLink = inactiveLink + " bg-white text-blue-900 rounded-l-lg ";
+    const router = useRouter();
+    const {pathname} = router;
+
     return (
-        <aside className="text-white p-1">
-            <a className="flex">
+        <aside className="text-white p-4 pr-0">
+            <Link href={'/'} className="flex gap-1 mb-4 mr-2">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605" />
                 </svg>
-                <span className="">
-                    Pawsome - Panel de administrador
+                <span className="flex">
+                    Pawsome - Admin Panel
                 </span>
-            </a>
-            <nav>
-                <Link href={'/'}>Dashboard</Link>
+            </Link>
+            <nav className="flex flex-col gap-2">
+                <Link href={'/'} className={pathname === '/' ? activeLink: inactiveLink}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                    </svg>
+
+                    Panel
+                </Link>
+
+                <Link href={'/products'} className={pathname.includes('/products') ? activeLink: inactiveLink}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                </svg>
+
+                    Productos
+                </Link>
+
+                <Link href={'/orders'} className={pathname.includes('/orders') ? activeLink: inactiveLink}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9 3.75H6.912a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859M12 3v8.25m0 0-3-3m3 3 3-3" />
+                </svg>
+
+
+                    Órdenes
+                </Link>
+
+                <Link href={'/settings'} className={pathname.includes('/settings') ? activeLink: inactiveLink}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12a7.5 7.5 0 0 0 15 0m-15 0a7.5 7.5 0 1 1 15 0m-15 0H3m16.5 0H21m-1.5 0H12m-8.457 3.077 1.41-.513m14.095-5.13 1.41-.513M5.106 17.785l1.15-.964m11.49-9.642 1.149-.964M7.501 19.795l.75-1.3m7.5-12.99.75-1.3m-6.063 16.658.26-1.477m2.605-14.772.26-1.477m0 17.726-.26-1.477M10.698 4.614l-.26-1.477M16.5 19.794l-.75-1.299M7.5 4.205 12 12m6.894 5.785-1.149-.964M6.256 7.178l-1.15-.964m15.352 8.864-1.41-.513M4.954 9.435l-1.41-.514M12.002 12l-3.75 6.495" />
+                </svg>
+
+                    Configuración
+                </Link>
             </nav>
         </aside>
     );
--- admin-panel-pawsome/pages/index.js
+++ admin-panel-pawsome/pages/index.js
@@ -1,23 +1,21 @@
-import Nav from "@/components/Nav";
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
+export default function Home(){
+  const {data: session} = useSession();
 
-export default function Home() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-    <div className= "bg-blue-900 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">Inicia sesión con Google</button>
+  return <Layout>
+    <div className="text-blue-900 flex justify-between">
+      <h2>
+        Hola, <b>{session?.user?.name}</b>
+      </h2>
+
+      <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+        <img src={session?.user?.image} alt="Imagen del usuario" className="w-6 h-6"/>
+        <span className="px-2">
+          {session?.user?.name}
+        </span>
       </div>
     </div>
-    );
-  }
-
-  return (
-    <div className="bg-blue-900 min-h-screen">
-      <Nav />
-      <div>Ha iniciado sesión {session.user.email}</div>
-    </div>
-  );
+  </Layout>
 }
