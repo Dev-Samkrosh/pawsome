3afc3eba1465f9d6f2aa3406c3803587e3f37755
Dev-Samkrosh <samuelbp3005@gmail.com>
2024-04-01 13:55:49 -0500

    Creada App para Front-end

--- admin-panel-pawsome/pages/products/delete/[...id].js
+++ admin-panel-pawsome/pages/products/delete/[...id].js
@@ -24,13 +24,13 @@ export default function DeleteProductPage() {
   }
   return (
     <Layout>
-      <h1 className="text-center">Do you really want to delete
+      <h1 className="text-center">¿Enserio quieres eliminar
         &nbsp;&quot;{productInfo?.title}&quot;?
       </h1>
       <div className="flex gap-2 justify-center">
         <button
           onClick={deleteProduct}
-          className="btn-red">Yes</button>
+          className="btn-red">Sí</button>
         <button
           className="btn-default"
           onClick={goBack}>
